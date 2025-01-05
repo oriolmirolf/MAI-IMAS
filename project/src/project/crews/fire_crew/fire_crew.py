c0c8d367bb1fe6a8b9afcb5d40b9cc41e3e46106
Oriol <mirolopezfeliuoriol@gmail.com>
2025-01-05 17:55:41 +0100

    material firefighters

--- project/src/project/crews/fire_crew/fire_crew.py
+++ project/src/project/crews/fire_crew/fire_crew.py
@@ -4,114 +4,156 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import FileReadTool
 from pydantic import BaseModel, Field
 from typing import List, Tuple
-
+from tools.route_distance_tool import RouteDistanceTool
 
 
 class FirefighterPlannerSchema(BaseModel):
-	"""Output for the firefighter plan task"""
-	# TODO: Identification or type of vehicle. More than one vehicle of the same type?
-	personnel: List[Tuple[str, int]] = Field(..., description='Pairs of personnel types and its number of units')
-	#personnel: List[(str, str)] = Field(..., description='Pairs of personnel identifications with its vehicle identification assigned.')
-	vehciles: List[Tuple[str, int]] = Field(..., description='Pairs of vehicle types and its quantity')
-	material: List[Tuple[str, int]] = Field(..., description='Pairs of equipment that must be carried to assess the fire, along with its quanitity.')
-	route_to_fire: List[Tuple[float, float]] = Field(..., description='List with X and Y coordinates that form the route from the firefighter station to the fire incident location.')
-	response_time: float = Field(..., description='Time taken to go from the firefighter station to the fire incident location.')
+    """Output for the firefighter plan task"""
+    personnel: List[Tuple[str, int]] = Field(..., description='Pairs of personnel types and their number of units')
+    vehicles: List[Tuple[str, int]] = Field(..., description='Pairs of vehicle types and their quantities')
+    material: List[Tuple[str, int]] = Field(..., description='Pairs of equipment to be carried, along with their quantities.')
+    route_to_fire: List[Tuple[float, float]] = Field(..., description='List of (X, Y) coordinates forming the route from the firefighter station to the fire incident location.')
+    response_time: float = Field(..., description='Time taken to go from the firefighter station to the fire incident location.')
 
-	@classmethod
-	def get_schema(cls) -> str:
-		schema = '\n'
-		for field_name, field_instance in cls.model_fields.items():
-			schema += f'{field_name}, described as: {field_instance.description}\n'
-		return schema
-
+    @classmethod
+    def get_schema(cls) -> str:
+        schema = '\n'
+        for field_name, field_instance in cls.model_fields.items():
+            schema += f'{field_name}, described as: {field_instance.description}\n'
+        return schema
 
 
 @CrewBase
-class FirefighterCrew():
-	"""Firefighter crew"""
+class FirefighterCrew:
+    """Firefighter crew"""
 
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
 
-	def __init__(self, emergency_file, resources_file):
-		self._emergency_file = emergency_file
-		self._resources_file = resources_file
+    def __init__(self, emergency_file, resources_file):
+        self._emergency_file = emergency_file
+        self._resources_file = resources_file
+        self.path_file_map = '../../maps/vilanova_i_la_geltru.graphml'
 
-	@agent
-	def firefighter_divider_agent(self) -> Agent:
-		file_read_tool = FileReadTool(file_path=self._emergency_file)
-		resources_read_tool = FileReadTool(file_path=self._resources_file)
-		return Agent(
-			config=self.agents_config['firefighter_divider_agent'],
-			verbose=True,
-			allow_delegation=False,
-			llm='ollama/llama3.1',
-			tools=[file_read_tool, resources_read_tool],
-			max_iter=1,
-		)
-	
-	@agent
-	def fire_expert_agent(self) -> Agent:
-		return Agent(
-			config=self.agents_config['fire_expert_agent'],
-			verbose=True,
-			allow_delegation=False,
-			llm='ollama/llama3.1',
-			max_iter=1,
-		)
-	
-	@agent
-	def firefighter_planner_agent(self) -> Agent:
-		return Agent(
-			config=self.agents_config['firefighter_planner_agent'],
-			verbose=True,
-			allow_delegation=False,
-			llm='ollama/llama3.1',
-			max_iter=1,
-		)
+    @agent
+    def firefighter_divider_agent(self) -> Agent:
+        file_read_tool = FileReadTool(file_path=self._emergency_file)
+        resources_read_tool = FileReadTool(file_path=self._resources_file)
+        return Agent(
+            config=self.agents_config['firefighter_divider_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            tools=[file_read_tool, resources_read_tool],
+            max_iter=1,
+        )
 
+    @agent
+    def fire_expert_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['fire_expert_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            max_iter=1,
+        )
 
-	# To learn more about structured task outputs, 
-	# task dependencies, and task callbacks, check out the documentation:
-	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def firefighter_divider_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['firefighter_divider_task'],
-		)
-	
-	@task
-	def fire_expert_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['fire_expert_task'],
-		)
-	
-	@task
-	def firefighter_planner_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['firefighter_planner_task'],
-			output_pydantic=FirefighterPlannerSchema
-		)
-	
-	
+    @agent
+    def material_selector_agent(self) -> Agent:
+        file_read_tool = FileReadTool(file_path=self._emergency_file)
+        resources_read_tool = FileReadTool(file_path=self._resources_file)
+        return Agent(
+            config=self.agents_config['material_selector_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            tools=[file_read_tool, resources_read_tool],
+            max_iter=1,
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Emergency crew"""
-		# To learn how to add knowledge sources to your crew, check out the documentation:
-		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+    @agent
+    def material_navigator_agent(self) -> Agent:
+        distance_calculator_tool = RouteDistanceTool(self.path_file_map)
+        return Agent(
+            config=self.agents_config['material_navigator_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            tools=[distance_calculator_tool],
+            max_iter=1,
+        )
 
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-		)
+    @agent
+    def material_planner_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['material_planner_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            max_iter=1,
+        )
 
+    @agent
+    def firefighter_planner_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['firefighter_planner_agent'],
+            verbose=True,
+            allow_delegation=False,
+            llm='ollama/llama3.1',
+            max_iter=1,
+        )
+
+    # Tasks
+    @task
+    def firefighter_divider_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['firefighter_divider_task'],
+        )
+
+    @task
+    def fire_expert_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['fire_expert_task'],
+        )
+
+    @task
+    def material_select_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['material_select_task'],
+        )
+
+    @task
+    def material_navigate_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['material_navigate_task'],
+        )
+
+    @task
+    def material_plan_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['material_plan_task'],
+        )
+
+    @task
+    def firefighter_planner_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['firefighter_planner_task'],
+            output_pydantic=FirefighterPlannerSchema
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Firefighter crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,    # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+        )
 
 if __name__ == "__main__":
-	result = (
-			FirefighterCrew(sys.argv[1], sys.argv[2])
-			.crew()
-			.kickoff()
-	)
+    result = (
+        FirefighterCrew(sys.argv[1], sys.argv[2])
+        .crew()
+        .kickoff()
+    )
---
+++ project/src/project/crews/fire_crew/tools/route_distance_tool.py
@@ -0,0 +1,51 @@
+import networkx
+import osmnx as ox
+from crewai_tools import BaseTool
+from typing import Optional, Type
+from pydantic import BaseModel, Field
+
+class RouteDistanceSchema(BaseModel):
+    """Input for the RouteDistanceTool."""
+    origin_location: str = Field(..., description='Name of the origin location.')
+    destination_location: str = Field(..., description='Name of the destination location.')
+
+class RouteDistanceTool(BaseTool):
+    name: str = 'Route distance calculator'
+    description: str = 'A tool to find the driving route distance between an origin location and a destination location.'
+    args_schema: Type[BaseModel] = RouteDistanceSchema
+    city_map: networkx.classes.multidigraph.MultiDiGraph = None
+    
+    class Config:
+        arbitrary_types_allowed = True
+    
+    def __init__(self, city_map: str, **kwargs):
+        super().__init__(**kwargs)
+        if not city_map:
+            raise Exception('A valid city map path to a graphml file must be provided.')
+        self.city_map = ox.load_graphml(city_map)
+        self.city_map = ox.routing.add_edge_speeds(self.city_map)
+        self.city_map = ox.routing.add_edge_travel_times(self.city_map)
+    
+    def _run(self, *args, **kwargs) -> int:
+        if not args:
+            raise ValueError("No arguments provided to RouteDistanceTool. You must provide as arguments a dictionary with 'origin_location' and 'destination_location'.")
+        args = args[0]
+        print(f'Els inputs que estic rebent són')
+        print(args)
+        origin_location = args.get('origin_location')
+        x_origin = origin_location[1]
+        y_origin = origin_location[0]
+
+        destination_location = args.get('destination_location')
+        x_destination = destination_location[1]
+        y_destination = destination_location[0]
+        
+        return self._find_distance(x_origin, y_origin, x_destination, y_destination)
+
+    def _find_distance(self, x_origin: float, y_origin: float, x_destination: float, y_destination: float) -> int:
+        origin_node = ox.distance.nearest_nodes(self.city_map, X=x_origin, Y=y_origin)
+        destination_node = ox.distance.nearest_nodes(self.city_map, X=x_destination, Y=y_destination)
+        route = ox.shortest_path(self.city_map, origin_node, destination_node, weight='travel_time')
+        edge_lengths = ox.routing.route_to_gdf(self.city_map, route)['length']
+
+        return round(sum(edge_lengths))
